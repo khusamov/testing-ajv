46ed5c7f4cdacc14768c62e6d98c3a658e14fe6c
Khusamov Sukhrob <khusamov@yandex.ru>
2017-08-14 07:18:49 +0300

    Пока не работает наше ключевое слово nullable

--- index.js
+++ index.js
@@ -1,19 +1,45 @@
+var _ = require('lodash');
 var Ajv = require('ajv');
 var ajv = new Ajv({ allErrors: true });
 
 var firstSchema = {
     "id": "/folder2/first.json",
+	"nullable": true,
     "type": "string"
 };
 
 var secondSchema = {
     "id": "/folder1/second.json",
-    "type": "object",
+	"type": "object",
     "properties": {
         "first": { "$ref": "/folder2/first.json" }
     }
 };
 
+
+ajv.addKeyword('nullable', {
+	validate: function(schema, data) {
+		return schema ? data === null : true;
+	}, errors: false 
+});
+
+ajv.addKeyword('constant', { validate: function (schema, data) {
+	return typeof schema == 'object' && schema !== null
+		? _.eq(schema, data)
+		: schema === data;
+}, errors: false });
+
+var schema = { "constant": 2 };
+var validate = ajv.compile(schema);
+console.log(validate(2)); // true
+console.log(validate(3)); // false
+
+var schema = { "constant": { "foo": "bar" } };
+var validate = ajv.compile(schema);
+console.log(validate({foo: 'bar'})); // true
+console.log(validate({foo: 'baz'})); // false
+
+process.exit(0);
 
 
 
@@ -24,6 +50,9 @@ ajv.addSchema(secondSchema);
 var data1 = {
 	first: "fdsfdsfsd"
 };
+var data2 = {
+	first: null
+};
 
-var valid = ajv.validate(secondSchema, data1);
+var valid = ajv.validate(secondSchema, data2);
 console.log(valid);
